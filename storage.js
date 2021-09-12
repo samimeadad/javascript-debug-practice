24e2d92a6eed99e3908428fc38ab3dfc8329c1a6
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-09-12 21:54:14 +0600

    storage practice done

---
+++ storage.js
@@ -0,0 +1,35 @@
+const sami = {
+    name: {
+        firstName: 'Sami',
+        middleName: 'Meadad',
+        surName: 'Choudhury'
+    },
+    id: 101,
+    isMarried: true,
+    friends: [ 'Farmee', 'Arishamma', 'Aarash' ],
+    subjects: [ 'csc-101', 'mat-100', 'eng-101' ],
+    location: 'Uttara',
+    age: 40,
+    height: 66,
+    mark: 59.33,
+    skill: {
+        webDev: {
+            language: [ 'JavaScript', 'html', 'CSS' ],
+            framework: [ 'bootstrap', 'tailwind', 'reactjs' ],
+            ide: 'vs-code'
+        },
+        programming: [ 'c', 'c++', 'java', 'python' ]
+    }
+};
+
+const addToLocalStorage = () => {
+    localStorage.setItem( 'Student', JSON.stringify( sami ) );
+}
+const removeFromLocalStorage = () => {
+    localStorage.removeItem( 'Student' );
+}
+const consoleLocalStorage = () => {
+    const localStorageData = localStorage.getItem( 'Student' );
+    const localStorageObject = JSON.parse( localStorageData );
+    console.log( localStorageObject );
+}
